919168fc198011e1b677d938a7128aab91e48f9f
redmopag <redmopag@gmail.com>
2024-07-11 02:01:40 +0500

    Изменить Order и добавить OrderStatus

--- src/main/java/ufanet/test_task/coffee_order_system/models/Order.java
+++ src/main/java/ufanet/test_task/coffee_order_system/models/Order.java
@@ -1,41 +1,32 @@
 package ufanet.test_task.coffee_order_system.models;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import org.springframework.stereotype.Component;
+import lombok.Setter;
+import ufanet.test_task.coffee_order_system.events.OrderEvent;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Setter
 public class Order {
-    private int id;
-    private List<OrderEvent> orderEvents;
+    private final int id;
+    private int clientId;
+    private int employeeId;
+    private LocalDateTime issueExpectedTime;
+    private int productId;
+    private double productPrice;
+    private OrderStatus status;
+    private String cancelReason = "";
 
-    public String getCurrentStatus(){
-        if(orderEvents.isEmpty()){
-            return "Not Ordered";
-        } else{
-            OrderEvent lastEvent = orderEvents.get(orderEvents.size()-1);
-            return lastEvent.getClass().getSimpleName()
-                    .replace("Event", "")
-                    .replace("Order", "");
+    private final List<OrderEvent> events;
+
+    public Order(int id, List<OrderEvent> events) {
+        this.id = id;
+        this.events = events;
+        for(OrderEvent event : events)
+        {
+            event.applyToAggregate(this);
         }
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", orderEvents=" + orderEvents +
-                '}';
-    }
-
-    public List<String> getEventsSummary(){
-        return orderEvents.stream()
-                .map(el -> String.format("%s at %s", el.getClass().getSimpleName(),el.getEventDateTime().toString()))
-                .toList();
     }
 }
---
+++ src/main/java/ufanet/test_task/coffee_order_system/models/OrderStatus.java
@@ -0,0 +1,9 @@
+package ufanet.test_task.coffee_order_system.models;
+
+public enum OrderStatus {
+    REGISTERED,
+    TAKEN,
+    READY,
+    ISSUED,
+    CANCELED;
+}
